54930724311908737b646172bf3041c4aba19047
Mochamad Fadlan Abdillah <mfadlanabdillah@outlook.com>
2025-02-20 22:33:52 +0700

    feat: Resource for Listing

---
+++ app/Filament/Resources/ListingResource/Pages/EditListing.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Filament\Resources\ListingResource\Pages;
+
+use App\Filament\Resources\ListingResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditListing extends EditRecord
+{
+    protected static string $resource = ListingResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+            Actions\ForceDeleteAction::make(),
+            Actions\RestoreAction::make(),
+        ];
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
